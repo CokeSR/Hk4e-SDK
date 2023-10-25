33f9e5e2ec978da0d4e82fe58293b814d62dba50
浮梦 <114860867+Fumeng24@users.noreply.github.com>
2023-10-25 16:48:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from settings.checkstatus import check_region , check_dispatch , check_config , check_mysql_connection , check_database_exists,check_muipserver
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
-#======================Falsk(main)======================#
+#======================Flask(main)======================#
 def start_flask_server(config):
     app.secret_key = config["Setting"]["secret_key"]
     app.debug = config["Setting"]["debug"]
