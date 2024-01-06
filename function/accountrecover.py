44266aab5b0f59ec68f5377367ac6e51a50d9a0a
longhuan1999 <2114467924@qq.com>
2024-01-06 13:02:11 +0800

    用session重写电子邮件验证码相关代码

--- function/accountrecover.py
+++ function/accountrecover.py
@@ -10,7 +10,8 @@ from settings.database import get_db
 from settings.library import password_hash
 from settings.loadconfig import get_config
 from settings.response import json_rsp_with_msg
-from flask import request, render_template, flash, current_app
+from flask import request, render_template, flash, current_app, session
+from datetime import datetime, timedelta
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 @app.context_processor
@@ -22,8 +23,9 @@ def inject_config():
 # 找回密码(功能不可用)
 @app.route('/account/recover', methods=['GET', 'POST'])
 def account_recover():
+    session.permanent = True
     cursor = get_db().cursor()
-    cached_data = cache.get(request.form.get('email'))
+    # cached_data = cache.get(request.form.get('email'))
     if request.method == 'POST':
         email = request.form.get('email')
         verifycode = request.form.get('verifycode')
@@ -36,8 +38,13 @@ def account_recover():
             email_exists = cursor.fetchone()
             if not email_exists:
                 flash('该邮箱不存在', 'error')
-            elif verifycode != cached_data and get_config()['Mail']['ENABLE']:
-                flash('验证码错误', 'error')
+            elif get_config()['Mail']['ENABLE'] and 'recover_codes' in session:
+                vaild = False
+                for recover_code_info in session['recover_codes']:
+                    if recover_code_info['email'] == email and recover_code_info['reset_code'] == verifycode and recover_code_info['vaild'] and recover_code_info["timeout"] >= datetime.now():
+                        vaild = True
+                if vaild is False:
+                    flash('验证码错误或失效', 'error')
             elif password != passwordv2:
                 flash('两次输入的密码不一致', 'error')
             elif len(password) < get_config()["Security"]["min_password_len"]:
@@ -53,6 +60,7 @@ def account_recover():
 # 邮件验证码 用于找回密码
 @app.route('/account/recover_code', methods=['POST'])
 def recover_code():
+    session.permanent = True
     email = request.form.get('email')
     email_pattern = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
     if not re.match(email_pattern, email):
@@ -63,6 +71,25 @@ def recover_code():
     user = cursor.fetchone()
     if not user:
         return json_rsp_with_msg(repositories.RES_FAIL, "该邮箱不存在", {})
+    if 'recover_codes' in session:
+        not_timeout_li = []
+        except_time = datetime.max
+        for n in range(len(session['recover_codes'])):
+            recover_code_info = session['recover_codes'][n]
+            if recover_code_info["timeout"] >= datetime.now():
+                # 计数未超时的验证码，包括因为同一email重复获取而失效的
+                not_timeout_li.append(n)
+            else:
+                # 删除超时的验证码
+                del session['recover_codes'][n]
+        if len(not_timeout_li) > 5:
+            # 当未超时的验证码超过5个后限制发送新的验证码
+            except_time = session['recover_codes'][not_timeout_li[-6]]["timeout"].strftime("%Y-%m-%d %H:%M:%S")
+            return json_rsp_with_msg(repositories.RES_FAIL, f"发送验证码频率超过限制，请在{except_time}后再试", {})
+    if 'send_code_timeout' in session and session['send_code_timeout'] < datetime.now():
+        # 验证与上次成功发送验证码的间隔是否超过60秒
+        except_time = session['send_code_timeout'].strftime("%Y-%m-%d %H:%M:%S")
+        return json_rsp_with_msg(repositories.RES_FAIL, f"发送验证码间隔为60秒，请在{except_time}后再试", {})
     reset_code = ''.join(random.choices(string.digits, k=4))
     mail = current_app.extensions['mail']
     msg = Message(f"重置密码请求", recipients=[email])
@@ -71,5 +98,23 @@ def recover_code():
         mail.send(msg)
     except:
         return json_rsp_with_msg(repositories.RES_FAIL, "未知异常，请联系管理员", {})
-    cache.set(email, reset_code, timeout=60 * 5)
+    recover_code_info = {
+            "email": email,
+            "verification_code": reset_code,
+            "timeout": datetime.now() + timedelta(seconds=300),
+            "valid": True
+        }
+    # 添加已发送验证码记录
+    if 'recover_codes' in session:
+        for n in range(len(session['recover_codes'])):
+            recover_code_info = session['recover_codes'][n]
+            if recover_code_info['email'] == email:
+                # 将同一个email下的其他验证码标记为失效
+                session['recover_codes'][n]['vaild'] = False
+        session['recover_codes'].append(recover_code_info)
+    else:
+        session['recover_codes'] = [recover_code_info,]
+    # 设置下次可以发送验证码的时间
+    session['send_code_timeout'] = datetime.now() + timedelta(seconds=60)
+    # cache.set(email, reset_code, timeout=60 * 5)
     return json_rsp_with_msg(repositories.RES_SUCCESS, "验证码发送成功，请查收邮箱", {})
--- function/accountregister.py
+++ function/accountregister.py
@@ -11,7 +11,8 @@ from settings.database import get_db
 from settings.library import password_hash
 from settings.loadconfig import get_config
 from settings.response import json_rsp_with_msg
-from flask import request, render_template, flash, current_app
+from flask import request, render_template, flash, current_app, session
+from datetime import datetime, timedelta
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 @app.context_processor
@@ -24,8 +25,9 @@ def inject_config():
 @app.route('/account/register', methods=['GET', 'POST'])
 @app.route('/mihoyo/common/accountSystemSandboxFE/index.html', methods=['GET', 'POST'])         # 国内沙箱 注册和找回URL是同一个
 def account_register():
+    session.permanent = True
     cursor = get_db().cursor()
-    cached_data = cache.get(request.form.get('email'))
+    # cached_data = cache.get(request.form.get('email'))
     if request.method == 'POST':
         username = request.form.get('username')
         mobile = request.form.get('mobile')
@@ -49,8 +51,13 @@ def account_register():
             flash('邮箱格式不正确', 'error')
         elif not re.fullmatch(r'^\d{11}$', mobile):
             flash('手机号码格式不正确', 'error')
-        elif verifycode != cached_data and get_config()['Mail']['ENABLE']:
-            flash('验证码错误', 'error')
+        elif get_config()['Mail']['ENABLE'] and 'register_codes' in session:
+            vaild = False
+            for register_code_info in session['register_codes']:
+                if register_code_info['email'] == email and register_code_info['verification_code'] == verifycode and register_code_info['vaild'] and register_code_info["timeout"]>=datetime.now():
+                    vaild = True
+            if vaild is False:
+                flash('验证码错误或失效', 'error')
         elif password != passwordv2:
             flash('两次输入的密码不一致', 'error')
         elif len(password) < get_config()["Security"]["min_password_len"]:
@@ -66,6 +73,7 @@ def account_register():
 # 邮件验证码 用于注册
 @app.route('/account/register_code', methods=['POST'])
 def register_code():
+    session.permanent = True
     email = request.form.get('email')
     email_pattern = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
     if not re.match(email_pattern, email):
@@ -76,6 +84,25 @@ def register_code():
     user = cursor.fetchone()
     if user:
         return json_rsp_with_msg(repositories.RES_FAIL, "邮箱已经被注册了", {})
+    if 'register_codes' in session:
+        not_timeout_li = []
+        except_time = datetime.max
+        for n in range(len(session['register_codes'])):
+            register_code_info = session['register_codes'][n]
+            if register_code_info["timeout"] >= datetime.now():
+                # 计数未超时的验证码，包括因为同一email重复获取而失效的
+                not_timeout_li.append(n)
+            else:
+                # 删除超时的验证码
+                del session['register_codes'][n]
+        if len(not_timeout_li) > 5:
+            # 当未超时的验证码超过5个后限制发送新的验证码
+            except_time = session['register_codes'][not_timeout_li[-6]]["timeout"].strftime("%Y-%m-%d %H:%M:%S")
+            return json_rsp_with_msg(repositories.RES_FAIL, f"发送验证码频率超过限制，请在{except_time}后再试", {})
+    if 'send_code_timeout' in session and session['send_code_timeout'] < datetime.now():
+        # 验证与上次成功发送验证码的间隔是否超过60秒
+        except_time = session['send_code_timeout'].strftime("%Y-%m-%d %H:%M:%S")
+        return json_rsp_with_msg(repositories.RES_FAIL, f"发送验证码间隔为60秒，请在{except_time}后再试", {})
     verification_code = ''.join(random.choices(string.digits, k=4))
     mail = current_app.extensions['mail']
     msg = Message(f"注册验证", recipients=[email])
@@ -84,5 +111,23 @@ def register_code():
         mail.send(msg)
     except:
         return json_rsp_with_msg(repositories.RES_FAIL, "未知异常，请联系管理员", {})
-    cache.set(email, verification_code, timeout=60*5)
+    register_code_info = {
+            "email": email,
+            "verification_code": verification_code,
+            "timeout": datetime.now() + timedelta(seconds=300),
+            "valid": True
+        }
+    # 添加已发送验证码记录
+    if 'register_codes' in session:
+        for n in range(len(session['register_codes'])):
+            register_code_info = session['register_codes'][n]
+            if register_code_info['email'] == email:
+                # 将同一个email下的其他验证码标记为失效
+                session['register_codes'][n]['vaild'] = False
+        session['register_codes'].append(register_code_info)
+    else:
+        session['register_codes'] = [register_code_info,]
+    # 设置下次可以发送验证码的时间
+    session['send_code_timeout'] = datetime.now() + timedelta(seconds=60)
+    #cache.set(email, verification_code, timeout=60*5)
     return json_rsp_with_msg(repositories.RES_SUCCESS, "验证码发送成功，请查收邮箱", {})
