e6f560a1455874392b82c19035189b3e84ce1e64
CokeSR <107022509+CokeSR@users.noreply.github.com>
2024-07-03 15:36:33 +0800

    修复编译相同proto时的字段冲突问题

--- protobuild/build.py
+++ protobuild/build.py
@@ -11,11 +11,11 @@ def convert_proto_to_python(proto_folder, output_folder, protoc):
     for root, dirs, files in os.walk(proto_folder):
         for file in files:
             if file.endswith(".proto"):
-                proto = os.path.join(root, file)
+                proto_path = os.path.join(root, file)
                 output_file = file.replace(".proto", "_pb2.py")
-                output = os.path.join(output_folder, output_file)
-                command = [protoc, f"--python_out={output_folder}", f"--proto={proto_folder}", proto]
-                subprocess.run(command, shell=True, check=True)
+                output_path = os.path.join(output_folder, output_file)
+                command = [protoc, f"--python_out={output_folder}", f"--proto_path={proto_folder}", proto_path]
+                subprocess.run(command, check=True)
 
 if __name__ == "__main__":
     convert_proto_to_python(proto_folder, output_folder, protoc)
