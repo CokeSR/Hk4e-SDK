ee1e5863a06ec2d5c6172194d1810ba49f18575a
CokeSR <107022509+CokeSR@users.noreply.github.com>
2024-07-03 18:34:28 +0800

    增加 CBT 对 dispatch 的分区访问

--- function/dispatch.py
+++ function/dispatch.py
@@ -2,9 +2,11 @@ try:
     from __main__ import app
 except ImportError:
     from main import app
-import sys
+
+import json
 import settings.repositories as repositories
-import data.proto.QueryRegionListHttpRsp_pb2 as RegionList
+import data.proto.cbt.QueryRegionListHttpRsp_v1_pb2 as RegionList_CBT
+import data.proto.live.QueryRegionListHttpRsp_v2_pb2 as RegionList_Live
 
 from base64 import b64encode
 from flask_caching import Cache
@@ -23,46 +25,91 @@ with open(repositories.DISPATCH_KEY, 'rb') as f:
 with open(repositories.DISPATCH_SEED, 'rb') as f:
     dispatch_seed = f.read()
 
-#=====================Dispatch配置=====================#
-# 实验性分区 Dispatch
-# version=CNRELWin4.0.1 lang=2 platform=3 binary=1 time=291 channel_id=1 sub_channel_id=1
-# version=OSRELWin2.8.0 lang=2 platform=3 binary=1 time=454 channel_id=1 sub_channel_id=1
+#===================== Dispatch 配置 =====================#
+# 实验性分区 Dispatch - CBT/Live
+# ?version=CHNWINCB1.0.0&lang=1&platform=3&binary=1&time=431
+# ?version=OVSWIN0.7.1&lang=1&platform=3&binary=1&time=429
+# ?version=CNRELWin4.0.1&lang=2&platform=3&binary=1&time=291&channel_id=1 sub_channel_id=1
+# ?version=OSRELWin2.8.0&lang=2&platform=3&binary=1&time=454&channel_id=1&sub_channel_id=1
 @app.route('/query_region_list', methods=['GET'])
 def query_dispatch():
-    response = RegionList.QueryRegionListHttpRsp()
-    response.retcode = repositories.RES_SUCCESS
-    for entry in check_config_exists()['Gateserver']:
-        if ('name' not in entry or not entry['name'] or
-            'title' not in entry or not entry['title'] or
-            'dispatchUrl' not in entry or not entry['dispatchUrl']):
-            print("#=====================Region读取失败！请检查[Config]配置=====================#")       # 客户端请求的时候再检查一下 如果不符合就原地抛锚
-            sys.exit(1)
-        region_info = response.region_list.add()
-        region_info.name = entry['name']
-        region_info.title = entry['title']
-        region_info.type = "PRODUTC"
-        region_info.dispatch_url = entry['dispatchUrl']
-        version = request.args.get('version')
-    if version.startswith('CNREL'):
-        custom_config = '{"sdkenv":"0","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}'
-    elif version == 'CNCB':
-        custom_config = '{"sdkenv":"6","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}'
-    elif version == 'OSCB':
-        custom_config = '{"sdkenv":"7","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}'
+    version = request.args.get('version', '')
+    if version.startswith('CHN'):
+        # CBT1
+        response = {
+            "region_list": [],
+            "clientCustomConfig":"{\"visitor\": false, \"sdkenv\": \"2\", \"checkdevice\": false}"
+        }
+
+        gateservers = check_config_exists().get('Gateserver', [])
+        for entry in gateservers:
+            region_info = {
+                "name": entry.get('name', ''),
+                "title": entry.get('title', ''),
+                "type": "DEV_PUBLIC",
+                # 草 有坑
+                "dispatchUrl": entry.get('dispatchUrl', '')
+            }
+            response["region_list"].append(region_info)
+
+        json_response = json.dumps(response)
+        return Response(json_response, content_type='text/plain')
+    elif version.startswith('OVS'):
+        # CBT2
+        custom_config = '{"sdkenv": "2", "checkdevice": false, "showexception": false}'
+        
+        response = RegionList_CBT.QueryRegionListHttpRsp_v1()
+        response.retcode = repositories.RES_SUCCESS
+        gateservers = check_config_exists().get('Gateserver', [])
+        
+        for entry in gateservers:
+            region_info = response.region_list.add()
+            region_info.name = entry.get('name', '')
+            region_info.title = entry.get('title', '')
+            region_info.type = "DEV_PUBLIC"
+            region_info.dispatch_url = entry.get('dispatchUrl', '')
+        
+        updated_region_list = RegionList_CBT.QueryRegionListHttpRsp_v1()
+        updated_region_list.region_list.extend(response.region_list)
+        updated_region_list.client_custom_config = custom_config
+        
+        serialized_data = updated_region_list.SerializeToString()
+        base64_str = b64encode(serialized_data).decode()
+        return Response(base64_str, content_type='text/plain')
     else:
-        custom_config = '{"sdkenv":"2","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}'
-    encrypted_config = bytearray()
-    for coke in range(len(custom_config)):
-        encrypted_config.append(ord(custom_config[coke]) ^ dispatch_key[coke % len(dispatch_key)])
-    updated_region_list = RegionList.QueryRegionListHttpRsp()
-    updated_region_list.region_list.extend(response.region_list)
-    updated_region_list.client_secret_key = dispatch_seed
-    updated_region_list.client_custom_config_encrypted = bytes(encrypted_config)
-    updated_region_list.enable_login_pc = True      # 试过了 无论是否都没用 但是必须要存在 :)
-    # 序列化
-    serialized_data = updated_region_list.SerializeToString()
-    base64_str = b64encode(serialized_data).decode()
-    return Response(base64_str, content_type='text/plain')
+        # CBT3 & live
+        version_config = {
+            'CNREL': '{"sdkenv":"0","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}',
+            'OSREL': '{"sdkenv":"5","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}',
+            'CNCB': '{"sdkenv":"6","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}',
+            'OSCB': '{"sdkenv":"7","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}'
+        }
+        default_config = '{"sdkenv":"2","checkdevice":"false","loadPatch":"false","showexception":"false","regionConfig":"pm|fk|add","downloadMode":"0"}'
+        
+        response = RegionList_Live.QueryRegionListHttpRsp_v2()
+        response.retcode = repositories.RES_SUCCESS
+        gateservers = check_config_exists().get('Gateserver', [])
+        
+        for entry in gateservers:
+            region_info = response.region_list.add()
+            region_info.name = entry.get('name', '')
+            region_info.title = entry.get('title', '')
+            region_info.type = "PRODUCT"
+            region_info.dispatch_url = entry.get('dispatchUrl', '')
+
+        version = request.args.get('version', '')
+        custom_config = version_config.get(version, default_config)
+        encrypted_config = bytearray(ord(char) ^ dispatch_key[idx % len(dispatch_key)] for idx, char in enumerate(custom_config))
+        
+        updated_region_list = RegionList_Live.QueryRegionListHttpRsp_v2()
+        updated_region_list.region_list.extend(response.region_list)
+        updated_region_list.client_secret_key = dispatch_seed
+        updated_region_list.client_custom_config_encrypted = bytes(encrypted_config)
+        updated_region_list.enable_login_pc = True
+        
+        serialized_data = updated_region_list.SerializeToString()
+        base64_str = b64encode(serialized_data).decode()
+        return Response(base64_str, content_type='text/plain')
 
 # 实验性解析QueryCurRegion(live版本服务端dispatchurl必须要填写sdk的地址，通过sdk来传递区服信息)
 # version=OSRELWin2.8.0 lang=2 platform=3 binary=1 time=348 channel_id=1 sub_channel_id=1 account_type=1 dispatchSeed=caffbdd6d7460dff key_id=3
